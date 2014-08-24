554c4080141368e0447cacf2f6ab9e80579ced17
Sahil Shah <sahilshah15@gmail.com>
2014-08-24 18:33:03 -0400

    second deploy

--- mysite.rb
+++ mysite.rb
@@ -3,3 +3,11 @@ require 'sinatra'
 get '/' do
   send_file 'public/index.html'
 end
+
+get '/contact' do
+	send_file 'public/contact.html'
+end
+
+get '/projects' do
+	send_file 'public/projects.html'
+end
